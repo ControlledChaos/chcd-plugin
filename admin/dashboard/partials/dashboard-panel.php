81c70cf501852cdff9a7e571c022028aae33cc1f
ControlledChaos <greg@ccdzine.com>
2020-11-29 02:58:44 -0800

    Dashboard CSS & HTML.

--- admin/dashboard/partials/dashboard-panel.php
+++ admin/dashboard/partials/dashboard-panel.php
@@ -37,105 +37,11 @@ $subheading = apply_filters( 'chcd_welcome_subheading', $subheading );
 $about_desc = apply_filters( 'chcd_welcome_about', __( 'Put your welcome message here.', chcd_plugin() :: DOMAIN ) );
 
 ?>
-<?php do_action( 'chcd_before_welcome_panel_content' ); ?>
-<div class="welcome-panel-content custom">
-	<?php do_action( 'chcd_welcome_panel_content_before' ); ?>
 
-	<?php echo sprintf(
-		'<h2 class="welcome-panel-title">%1s %2s.</h2>',
-		esc_html__( 'Welcome,', chcd_plugin() :: DOMAIN ),
-		$user_name
-	); ?>
-	<p class="welcome-panel-description"><?php echo $about_desc; ?></p>
-	<?php echo $subheading; ?>
-	<p><?php _e( 'Use this to provide handy links to manage content, informational widgets, or maybe an instructional video.' ); ?></p>
-	<p><?php _e( 'No CSS has been applied to this welcome panel. Add styles as necessary for your project.' ); ?></p>
-
-	<div class="welcome-panel-column-container">
-		<?php do_action( 'chcd_welcome_panel_column_container_before' ); ?>
-
-		<div class="welcome-panel-column">
-			<?php do_action( 'chcd_welcome_panel_column_first_before' ); ?>
-
-				<?php if ( is_active_sidebar( 'chcd_welcome_widget_first' ) ) {
-
-					dynamic_sidebar( 'chcd_welcome_widget_first' );
-
-				} else {
-
-					$placeholder = sprintf(
-						'<h3>%1s</h3>',
-						esc_html( 'Column One', chcd_plugin() :: DOMAIN )
-					);
-					$placeholder .= sprintf(
-						'<p><a href="%1s">%2s</a> %3s.</p>',
-						admin_url( 'widgets.php' ),
-						__( 'Add a widget', chcd_plugin() :: DOMAIN ),
-						__( 'to this area', chcd_plugin() :: DOMAIN )
-					);
-
-					echo $placeholder;
-
-				} ?>
-
-			<?php do_action( 'chcd_welcome_panel_column_first_after' ); ?>
-		</div>
-		<div class="welcome-panel-column">
-			<?php do_action( 'chcd_welcome_panel_column_second_before' ); ?>
-
-			<?php if ( is_active_sidebar( 'chcd_welcome_widget_second' ) ) {
-
-					dynamic_sidebar( 'chcd_welcome_widget_second' );
-
-				} else {
-
-					$placeholder = sprintf(
-						'<h3>%1s</h3>',
-						esc_html( 'Column Two', chcd_plugin() :: DOMAIN )
-					);
-					$placeholder .= sprintf(
-						'<p><a href="%1s">%2s</a> %3s.</p>',
-						admin_url( 'widgets.php' ),
-						__( 'Add a widget', chcd_plugin() :: DOMAIN ),
-						__( 'to this area', chcd_plugin() :: DOMAIN )
-					);
-
-					echo $placeholder;
-
-				} ?>
-
-			<?php do_action( 'chcd_welcome_panel_column_second_after' ); ?>
-		</div>
-		<div class="welcome-panel-column welcome-panel-last">
-			<?php do_action( 'chcd_welcome_panel_column_last_before' ); ?>
-
-			<?php if ( is_active_sidebar( 'chcd_welcome_widget_last' ) ) {
-
-					dynamic_sidebar( 'chcd_welcome_widget_last' );
-
-				} else {
-
-					$placeholder = sprintf(
-						'<h3>%1s</h3>',
-						esc_html( 'Column Three', chcd_plugin() :: DOMAIN )
-					);
-					$placeholder .= sprintf(
-						'<p><a href="%1s">%2s</a> %3s.</p>',
-						admin_url( 'widgets.php' ),
-						__( 'Add a widget', chcd_plugin() :: DOMAIN ),
-						__( 'to this area', chcd_plugin() :: DOMAIN )
-					);
-
-					echo $placeholder;
-
-				} ?>
-
-			<?php do_action( 'chcd_welcome_panel_column_last_after' ); ?>
-		</div>
-
-		<?php do_action( 'chcd_welcome_panel_column_container_after' ); ?>
-	</div>
-
-	<?php do_action( 'chcd_welcome_panel_content_after' ); ?>
-</div>
-<?php do_action( 'chcd_after_welcome_panel_content' ); ?>
+<?php echo sprintf(
+	'<h2 class="welcome-panel-title">%1s %2s.</h2>',
+	esc_html__( 'Welcome,', chcd_plugin() :: DOMAIN ),
+	$user_name
+); ?>
+<p class="welcome-panel-description"><?php echo $about_desc; ?></p>
+<?php echo $subheading; ?>
